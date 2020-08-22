d9a37180c1bb1273950e50d3fc15814d14157874
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2020-08-23 08:51:24 +0900

    my-page create <users-table>
    rails g model userで
    db/migrate/20200822223251_create_users.rbを作成
    create_users.rbREADMEの各種項目１２個を記載。
    rails db:migrateを実行。sequel proで12項目と別途ID欄、created_atとcreated_atが作成（後者２つはtimestampsから）

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'users/index'
   # ルーティングの変更(デプロイ勉強会時)
   root 'items#index'
+
+  resources :users, only: [:index]
 end
---
+++ db/migrate/20200822223251_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :nickname,              null: false
+      t.string :mail,                  null: false, unique: true
+      t.string :pass,                  null: false, unique: true      
+      t.string :confirmation_pass,     null: false, unique: true
+      t.string :family_name,           null: false
+      t.string :family_name_kana,      null: false
+      t.string :first_name,            null: false, unique: true
+      t.string :first_name_kana,       null: false, unique: true
+      t.integer :birth_year,           null: false
+      t.integer :birth_month,          null: false
+      t.integer :birth_day,            null: false
+      t.string :icon
+      t.timestamps
+    end
+  end
+end
