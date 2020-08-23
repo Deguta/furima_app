b42389f63ce08630d7fc174bc7aba343918c26c2
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2020-08-23 09:46:20 +0900

    master update (create <users table>)
    テーブル作成時に、first nameとそのkanaに
    unique: trueがありましたが、一意制約は必要ないので修正いたします。
    rollbackしてから再度migrateで作り直したので、制約は削除できているはずです。
    VScode上では変化が出ないと思いますが。

--- db/migrate/20200822223251_create_users.rb
+++ db/migrate/20200822223251_create_users.rb
@@ -7,8 +7,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :confirmation_pass,     null: false, unique: true
       t.string :family_name,           null: false
       t.string :family_name_kana,      null: false
-      t.string :first_name,            null: false, unique: true
-      t.string :first_name_kana,       null: false, unique: true
+      t.string :first_name,            null: false
+      t.string :first_name_kana,       null: false
       t.integer :birth_year,           null: false
       t.integer :birth_month,          null: false
       t.integer :birth_day,            null: false
