0f63f407f4ae0765d131d6de1eacdb12108ffe86
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-08-26 13:39:59 +0900

    update devise-page
    インデントの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  # バリデーションの設定(空の文字列を保存させない為と一意性制約)
-  validates :nickname,          presence: true
-  validates :mail,              presence: true, uniqueness: true
-  validates :pass,              presence: true
-  validates :confirmation_pass, presence: true
-  validates :family_name,       presence: true
-  validates :family_name_kana,  presence: true
-  validates :first_name,        presence: true
-  validates :first_name_kana,   presence: true
-  validates :birth_year,        presence: true
-  validates :birth_month,       presence: true
-  validates :birth_day,         presence: true
-
-
-  # アソシエーション
-  has_many :cards,          dependent: :destroy
-  has_many :shipping_infos, dependent: :destroy
-  has_many :comments,       dependent: :destroy
-  has_many :items
+    # バリデーションの設定(空の文字列を保存させない為と一意性制約)
+    validates :nickname,          presence: true
+    validates :mail,              presence: true, uniqueness: true
+    validates :pass,              presence: true
+    validates :confirmation_pass, presence: true
+    validates :family_name,       presence: true
+    validates :family_name_kana,  presence: true
+    validates :first_name,        presence: true
+    validates :first_name_kana,   presence: true
+    validates :birth_year,        presence: true
+    validates :birth_month,       presence: true
+    validates :birth_day,         presence: true
+    
+    # アソシエーション
+    has_many :cards,          dependent: :destroy
+    has_many :shipping_infos, dependent: :destroy
+    has_many :comments,       dependent: :destroy
+    has_many :items
 end
