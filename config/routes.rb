12dab634b6e4f6599703e15ee18b33cc33276917
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-08-27 19:41:39 +0900

    update devise-page
    ログイン&新規登録ボタンの画面遷移を設定と、サインアップ機能の実装。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # カスタムpathの生成
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # login/sign_inへのカスタムパス
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # sign_up/registrationへのカスタムパス
+  end
+
   devise_for :users
-  
-  # ルーティングの変更(デプロイ勉強会時)
   root 'items#index'
-  
-  resources :users, only: [:index]
+    resources :users, only: [:index]
 end
