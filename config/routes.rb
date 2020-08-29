2991b127cacb04c4b517902c13edbd70e330558c
Deguta <tech.ym1110@gmail.com>
2020-08-29 21:48:08 +0900

    update shipping-page
    routingの#で説明を書いたのみ

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users　#user登録に必要なコード
   
   # ルーティングの変更(デプロイ勉強会時)
   root 'items#index'
+  # root to:"items#index"でもOK
+  # 最初のトップページである。因みに#indexを#newに書き換えるとnewページになる
   
   resources :users, only: [:index]
+  # usersはusers_controllerを橋渡ししている。 [:index]はアクションコントローラーの処理名
+  # resouucesとコントローラー名前を増やす事により表示させるページを増やす事ができる
+
 
   resources :items, only: [:new]
+  # itemsはitems_controllerを橋渡ししている。 [:new]はアクションコントローラーの処理名
+  # 商品を新規登録するため[:new]を使用する
+
 end
