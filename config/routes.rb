b2bb31f34474fe9d76c911d66f276e26711f23b9
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2020-09-05 12:05:07 +0900

    my-page update
    cardsコントローラーの作成にて、クレジットカード情報を登録するためのページへと遷移できるようにした。
    作成後、ルートの設定を行い、cards/newで該当ページへと飛べるようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   # ルーティングの変更(デプロイ勉強会時)
   root 'items#index'
-  
+  root 'cards#new' 
+
   resources :users, only: [:index]
+  resources :cards, only: [:new]
 end
