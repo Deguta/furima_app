994f0ee28d584c7dd9c1be55f1791d52d98c79dc
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-09-06 19:04:09 +0900

    update login-test
    shipping_infoモデルへ正規表現追加

--- app/models/shipping_info.rb
+++ app/models/shipping_info.rb
@@ -5,9 +5,9 @@ class ShippingInfo < ApplicationRecord
   belongs_to :user, optional: true
 
   # バリデーションの設定(空の文字列を保存させない為と一意性制約)
-  validates :family_name,       presence: true
-  validates :family_name_kana,  presence: true
-  validates :first_name,        presence: true
-  validates :first_name_kana,   presence: true
+  validates :family_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name,        presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana,   presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :prefecture,        presence: true
 end
