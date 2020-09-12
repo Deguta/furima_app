2e73a00966290acf810d8d01fd9a9ce779336b64
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-09-12 14:27:43 +0900

    update master
    マイグレート実行で作成されたコード

--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,15 @@ ActiveRecord::Schema.define(version: 2020_09_08_233106) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-ActiveRecord::Schema.define(version: 2020_09_08_083448) do
-
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -78,10 +80,10 @@ ActiveRecord::Schema.define(version: 2020_09_08_083448) do
     t.string "family_name_kana", null: false
     t.string "first_name", null: false
     t.string "first_name_kana", null: false
-    t.integer "postal_code"
+    t.integer "postal_code", null: false
     t.integer "prefecture_id", null: false
-    t.string "city"
-    t.string "address"
+    t.string "city", null: false
+    t.string "address", null: false
     t.string "building"
     t.string "phone_number"
     t.bigint "user_id", null: false
@@ -98,15 +100,12 @@ ActiveRecord::Schema.define(version: 2020_09_08_083448) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
-    t.string "pass", null: false
-    t.string "confirmation_pass", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "family_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name", null: false
     t.string "first_name_kana", null: false
-    t.integer "birth_year", null: false
-    t.integer "birth_month", null: false
-    t.integer "birth_day", null: false
+    t.date "birth_day", null: false
     t.string "icon"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
