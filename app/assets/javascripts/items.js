e75d1ed64cc49168a5727518b32c345d7cffc9ca
yasuyoshi0608 <i>
2020-09-19 15:28:19 +0900

    update product-function
    jsのfileの間違いを修正

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,21 +1,21 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="item[images_attributes][${index}][src]"
-                    id="item_images_attributes_${index}_src"><br>
-                    <div class="js-remove">削除</div>
-                  </div>`;
+    const html = `<div class="js-file_group" data-index="${index}">
+    <input class="js-file" type="file" name="item[item_images_attributes][${index}][image]" id="item_item_images_attributes_${index}_image">
+    <br>
+    <span class="js-remove">削除</span>
+    </div>`;
     return html;
   }
 
+  console.log("ok");
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
-  $('#image-box').on('change', '.js-file', function(e) {
+  $('.shipping-page__box').on('change', '.js-file', function(e) {
     // fileIndexの先頭の数字を使ってinputを作る
-    $('#image-box').append(buildFileField(fileIndex[0]));
+    $('.js-file_group').append(buildFileField(fileIndex[0]));
     fileIndex.shift();
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
