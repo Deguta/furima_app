28fd7e1d5c1b13a9c2478d5eb80e16f76b3cc190
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2020-09-22 15:07:56 +0900

    update credit-card_pay.jp
    1、Payjpのキーを使うための準備
    　　credentials.ymlへのkey２つの書き込み
    ２、書き込んだキーを使うために、controller
    　　からその設定を記述

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,6 +2,11 @@ class CardsController < ApplicationController
   def new
   end
 
+  # Payjpのキーを使うための記述、最後のsecret_key
+  def index
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+  end
+
   def show
   end
 end
