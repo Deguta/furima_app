7051f123ed2a6c697aefb77261c01c8d133a18d4
Deguta <tech.ym1110@gmail.com>
2020-09-25 18:19:48 +0900

    update product-function
    画像の投稿のテスト機能を実装

--- spec/factories/item_images.rb
+++ spec/factories/item_images.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :item_image do
-    
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/sample.jpeg')) }
+    association :item
   end
 end
