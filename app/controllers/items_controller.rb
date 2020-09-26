1bb699fdbe0361213a0f99720830a5ce308e9d25
yasuyoshi0608 <i>
2020-09-26 18:43:59 +0900

    update items-edit
    dontrollerにedit updateを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,16 @@ before_action :set_item, except: [:index, :new, :create]
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
