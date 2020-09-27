51ed9fbd1fbf76fae09d42d3e5a9383b5bac1e8e
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2020-09-27 17:58:11 +0900

    update credit-card_pay.jp
    user_id         ---ログインユーザー
    customer_id---顧客ID（pay.jpとのやり取りで使用）
    card_id         ---クレジットカードが複数枚の時、判別できるように。

--- db/migrate/20200908083448_create_cards.rb
+++ db/migrate/20200908083448_create_cards.rb
@@ -1,8 +1,9 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.string     :customer_token, null: false
-      t.references :user,     forign_key:true
+      t.integer :user_id,     null: false
+      t.string  :customer_id, null: false
+      t.string  :card_id,     null: false
       t.timestamps
     end
   end
