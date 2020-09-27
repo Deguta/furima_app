0eccd66825c8321c4994832d81bd0c0807211fe7
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2020-09-27 17:55:02 +0900

    upadate credit-card_pay.jp
    まだ記述に足りないところが多い。
    主に、credentialに書き混む予定のAPIキー（公開鍵、秘密鍵）を使用する。

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,24 @@
 class CardsController < ApplicationController
-  def new
-  end
 
-  # Payjpのキーを使うための記述、最後のsecret_key
   def index
-    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+    @card = Card.new
   end
 
-  def show
+  def pay
   end
-end
+
+  def create
+    email = current_user.email
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+    customer = Payjp::Customer.create(  ## 顧客の作成
+      email: email,
+      card: params[:card_token]
+    )
+    @card = Card.new(customer_token: customer&.id)
+    redirect_to action: "new", alert: "カードの登録に失敗しました。" and return if @card.invalid?
+    ## 保存に成功した場合
+    @card.user_id = current_user.id
+    @card.save
+    redirect_to cards_path and return
+  end
+end  
