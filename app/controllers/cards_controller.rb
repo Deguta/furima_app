a245ce2c6e20aaf76294e243b6cf9fc493132559
Deguta <tech.ym1110@gmail.com>
2020-09-29 00:41:22 +0900

    update credit=card_pay.jp
    カード登録まで完了
    但しPayjp::AuthenticationErrorが出る

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,24 +1,53 @@
 class CardsController < ApplicationController
 
-  def index
-    @card = Card.new
+  require "payjp"
+
+  def new
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
   end
 
-  def pay
+  def pay #payjpとCardのデータベース作成を実施します。
+    Payjp.api_key = "pk_test_2651fdbcc6b851d22b1e773e"
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+        description: '登録テスト', #なくてもOK
+        email: current_user.email, #なくてもOK
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
+        ) #念の為metadataにuser_idを入れましたがなくてもOK
+      customer = Payjp::Customer
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
   end
 
-  def create
-    email = current_user.email
-    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
-    customer = Payjp::Customer.create(  ## 顧客の作成
-      email: email,
-      card: params[:card_token]
-    )
-    @card = Card.new(customer_token: customer&.id)
-    redirect_to action: "new", alert: "カードの登録に失敗しました。" and return if @card.invalid?
-    ## 保存に成功した場合
-    @card.user_id = current_user.id
-    @card.save
-    redirect_to cards_path and return
+  def delete #PayjpとCardデータベースを削除します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = "pk_test_2651fdbcc6b851d22b1e773e"
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new"
   end
-end  
+
+  def show #Cardのデータpayjpに送り情報を取り出します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = "pk_test_2651fdbcc6b851d22b1e773e"
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -80,10 +80,10 @@ ActiveRecord::Schema.define(version: 2020_09_08_233106) do
     t.string "family_name_kana", null: false
     t.string "first_name", null: false
     t.string "first_name_kana", null: false
-    t.integer "postal_code"
+    t.integer "postal_code", null: false
     t.integer "prefecture_id", null: false
-    t.string "city"
-    t.string "address"
+    t.string "city", null: false
+    t.string "address", null: false
     t.string "building"
     t.string "phone_number"
     t.bigint "user_id", null: false
