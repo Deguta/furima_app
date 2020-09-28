839a26690ccad29aa787232713a9f36d9044a87c
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-09-28 21:26:14 +0900

    update master
    テストコードの修正

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -3,5 +3,4 @@ class ItemImage < ApplicationRecord
     # アソシエーション
     mount_uploader :image, ImageUploader
     belongs_to :item
-
 end
--- spec/models/item_image_spec.rb
+++ spec/models/item_image_spec.rb
@@ -10,12 +10,6 @@ RSpec.describe ItemImage, type: :model do
         item_image = FactoryBot.build(:item_image)
         expect(item_image).to be_valid
       end
-
-      it "画像がなければ登録できないこと" do
-        item_image = build(:item_image, image: nil)
-        item_image.valid?
-        expect(item_image.errors[:image]).to include("を入力してください")
-      end
     end
   end
 end
