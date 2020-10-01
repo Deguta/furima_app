d956cc015bbb4feed4938a0ab5684f84ec4ad652
Deguta <tech.ym1110@gmail.com>
2020-10-01 15:49:16 +0900

    update credit-card_.pay.jp
    画像のサイズを変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,5 +44,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fit: [50, 50]
+  process resize_to_fit: [220, 200]
 end
