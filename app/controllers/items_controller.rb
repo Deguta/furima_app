fd2c65088f1e3a22086d83ad3d6520125c16b7c3
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2020-10-02 16:31:43 +0900

    upadate category_
    購入や、支払いなどのアクションをコントローラーに追加。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-before_action :set_item, except: [:index, :new, :create]
+before_action :set_item, only: [:show, :destroy, :edit, :update, :purchase, :payment]
 
   def index
     @items = Item.includes(:item_images).order('created_at DESC')
@@ -8,7 +8,27 @@ before_action :set_item, except: [:index, :new, :create]
   def new
     @item = Item.new
     @item.item_images.new
+
+    #セレクトボックスの初期値設定
+    @category_parent_array = ["---"]
+    #データベースから、親カテゴリーのみ抽出し、配列化
+    @category_parent_array = Category.where(ancestry: nil)
   end
+
+  # 以下全て、formatはjsonのみ
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    # ここでfind_byを使うことでレディーしか取れてなかった
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+    # 子カテゴリーが選択された後に動くアクション
+    def get_category_grandchildren
+      #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+      @category_grandchildren = Category.find(params[:child_id]).children
+    end
+  # end
 
   def show
   end
@@ -41,14 +61,13 @@ before_action :set_item, except: [:index, :new, :create]
     end
   end
 
-end
-
 private
 
-def item_params
-  params.require(:item).permit(:name, :description, :category, :brand, :condition, :shipping_cost, :prefecture_id, :shipping_day, :price, item_images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
-end
+  def item_params
+    params.require(:item).permit(:name, :description, :category, :brand, :condition, :shipping_cost, :prefecture_id, :shipping_day, :price, item_images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
+  end
 
-def set_item
-  @item = Item.find(params[:id])
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
